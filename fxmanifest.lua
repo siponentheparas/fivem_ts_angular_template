e2746651184e6bba03b466b5faedebf02c9a1717
siponen <70901003+siponentheparas@users.noreply.github.com>
2024-07-16 22:33:18 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'siponen'
+description 'desc'
+
+ui_page 'dist/web/browser/index.html'
+
+files {
+    'dist/web/browser/*'
+}
+
+client_scripts {
+    'dist/client/**/*.js'
+}
+
+server_scripts {
+    'dist/server/**/*.js'
+}
